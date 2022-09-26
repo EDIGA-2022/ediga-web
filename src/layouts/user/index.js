8fa31cdf15bdaf1797c70c3cc0519618eb1b9997
sofiaalberti <sofialberti2409@gmail.com>
2022-09-26 17:44:38 -0300

    Show only app ediga data in tab

--- src/layouts/user/index.js
+++ src/layouts/user/index.js
@@ -101,7 +101,7 @@ function User() {
         </div>
 
       })} */}
-      <MDBox mt={5} mb={3}>
+      {tabValue === 0 && <MDBox mt={5} mb={3}>
         <Grid container spacing={1}>
           {/* <Grid item xs={12} md={6} xl={4}>
             <PlatformSettings />
@@ -148,7 +148,7 @@ function User() {
             <ProfilesList title="Imagenes" photos={user.photos} shadow={true} />
           </Grid>
         </Grid>
-      </MDBox>
+      </MDBox>}
     </DashboardLayout>
   );
 };
